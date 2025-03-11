ee35b84a5240912f02e46789a4024b9a3432bb99
Amy <emysgalippa@gmail.com>
2025-03-11 15:15:52 +0100

    initialize project

--- index.ts
+++ index.ts
@@ -1,19 +1,11 @@
+import * as dotenv from "dotenv";
+dotenv.config();
 import * as express from "express";
-import * as path from "path";
-
 const app = express();
 const port = parseInt(process.env.PORT) || process.argv[3] || 8080;
 
-app.use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs');
-
-app.get('/', (req, res) => {
-  res.render('index');
-});
-
-app.get('/api', (req, res) => {
-  res.json({"msg": "Hello world"});
+app.get("/", (req, res) => {
+  res.json({ message: "Hello world" });
 });
 
 app.listen(port, () => {
